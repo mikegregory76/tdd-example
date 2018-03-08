c8f8a2d7452e4732321069d2e62fc193f9e54dc0
Mike Gregory <mgregory@mainstreetfamilycare.com>
2018-03-08 08:42:14 -0600

    fixing namespace

--- TddExample/TddExampleTests/RedTests.cs
+++ TddExample/TddExampleTests/RedTests.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TddExample;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TddExample.Tests
+namespace TddExampleTests
 {
     [TestClass()]
     public class RedTests
